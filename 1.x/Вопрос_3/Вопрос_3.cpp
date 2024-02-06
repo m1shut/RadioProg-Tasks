917a776d11c805469d2d77ecc287b55b76db2549
m1shut <shoshmisha@gmail.com>
2024-02-06 22:11:24 +0300

    1.x – Резюме к главе 1 и небольшой тест
    Глава 1 – Основы C++

---
+++ 1.x/Вопрос_3/Вопрос_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    
+    cout << "Enter an integer: ";
+    cin >> num1;
+    cout << "Enter another integer: ";
+    cin >> num2;
+    cout << num1 << " + " << num2 << " is " << num1 + num2 << ".\n";
+    cout << num1 << " - " << num2 << " is " << num1 - num2 << ".\n";
+
+    return 0;
+}
