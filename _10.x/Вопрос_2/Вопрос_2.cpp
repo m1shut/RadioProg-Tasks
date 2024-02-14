07fd3fad1c6d91482f1220aa70805e5ddbd4eae4
m1shut <shoshmisha@gmail.com>
2024-02-14 19:37:51 +0300

    10.x – Резюме к главе 10 и небольшой тест
    Глава 10 – Массивы, строки, указатели и ссылки

---
+++ _10.x/Вопрос_2/Вопрос_2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Base {
+	string name;
+	int rate;
+};
+
+int getNumStudents(){
+	int size;
+	cout << "How many student do you want to enter? ";
+	cin >> size;
+
+	return size;
+}
+
+vector<Base> getNameAndRateStudents() {
+	int studentsNum = getNumStudents();
+	
+	vector <Base> stud(static_cast<vector<Base>::size_type>(studentsNum));
+
+	for (auto& student : stud) {
+		cout << "Enter a student name: ";
+		cin >> student.name;
+
+		cout << "Enter a rate of student: ";
+		cin >> student.rate;
+	}
+
+	return stud;
+}
+
+bool studComapre(Base& a, Base& b) {
+	return a.rate > b.rate;
+}
+
+int main() {
+	auto stud = getNameAndRateStudents();
+
+	sort(stud.begin(), stud.end(), studComapre);
+
+	for (auto& student : stud) {
+		cout << student.name << " got a grade of " << student.rate << "\n";
+	}
+
+	return 0;
+}
+
+
---
+++ _10.x/Вопрос_4/Вопрос_4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void printText(const char* str) {
+	while (*str != '\0') {
+		cout << *str++;
+	}
+}
+
+int main() {
+	printText("Hello, world!");
+
+	return 0;
+}
+
