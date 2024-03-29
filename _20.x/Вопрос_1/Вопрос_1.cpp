f1cc245d81e4c82dd129603d6fba801afe117635
m1shut <shoshmisha@gmail.com>
2024-03-29 22:10:22 +0300

    20.x – Резюме к главе 20 и небольшой тест
    Глава 20 – Исключения

---
+++ _20.x/Вопрос_1/Вопрос_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class Fraction {
+private:
+    int denominator;
+    int numerator;
+public:
+    Fraction(int denominator, int numerator) {
+        this->denominator = denominator;
+        this->numerator = numerator;
+        
+        if (denominator == 0) throw runtime_error("Invalid denominator");
+    }
+};
+
+int main() {
+    cout << "Enter the numerator: ";
+    int numerator;
+    cin >> numerator;
+
+    cout << "Enter the denominator: ";
+    int denominator;
+    cin >> denominator;
+
+    try {
+        Fraction f(denominator, numerator);
+        cout << "Answer: " << numerator / denominator;
+    }
+    catch (exception ex) {
+        cout << ex.what();
+    }
+
+    return 0;
+}
+
+
